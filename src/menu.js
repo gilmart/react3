a90c1a291d72211535bd212dd5dfa98507901430
503 <503@academicos.cesde.edu.co>
2022-05-28 13:18:32 -0500

    react3

---
+++ src/menu.js
@@ -0,0 +1,23 @@
+import { Fragment } from "react";
+
+export function menu(){
+
+    return(
+
+        <>
+        <header>
+           <nav classname="navbar navbar-expand-lg bg-light">
+        <div classname="container-fluid">
+          <a classname="navbar-brand" href="#">Navbar</a>
+          <button classname="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span classname="navbar-toggler-icon" />
+          </button>
+          
+        </div>
+      </nav>
+
+      </header>
+        </>
+       
+    )
+}
